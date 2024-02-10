df88375e04a78fb7be5d8fee53ee286eefec437e
wsant <wsant@Trueno>
2024-02-10 14:52:53 -0500

    primer trabajo

---
+++ src/main/java/com/mycompany/ave/Ave.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ave;
+
+/**
+ *
+ * @author wsant
+ */
+public abstract class Ave {
+
+    public abstract void makeSound();
+    
+    
+}
